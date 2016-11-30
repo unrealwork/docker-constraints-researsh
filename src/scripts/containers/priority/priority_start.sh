954f575cf6761deacc88dfdfd2d407dd1501d9bc
Unrealwork <unrealwork@gmail.com>
2016-11-30 14:26:44 +0300

    some messages

--- src/scripts/containers/priority/priority_start.sh
+++ src/scripts/containers/priority/priority_start.sh
@@ -1,5 +1,30 @@
 #!/usr/bin/env bash
-rm -rf ~/myblog
-jekyll new myblog
-docker rm -f docker-nginx
-docker run --name docker-nginx -p 80:80 -d -v ~/myblog/_site/:/usr/share/nginx/html nginx
+constraint=$1;
+SITE_NAME="myblog"
+SITE_FOLDER="~/${SITE_NAME}"
+
+if [[ -z ${constraint} ]];then
+   echo "Start container without constarint";
+else
+    echo "Start priority container with constarint: ${constraint}";
+fi
+if [ -d ~/${SITE_FOLDER} ]; then
+  echo "Remove previous version of site";
+  rm -rf ~/${SITE_FOLDER};
+else
+  echo "There is no previous version of site";
+fi
+if [[ -z $(which jekyll) ]]; then
+    echo "You need to install jekyll generator!"
+    exit 1;
+else
+    jekyll new ${SITE_NAME}
+fi
+if [[ -z $(which docker) ]]; then
+    echo "You need to install docker generator!"
+    exit 1;
+else
+    docker rm -f docker-nginx &> /dev/null
+    docker run --name docker-nginx ${constraint} -p 80:80 -d -v ~/myblog/_site/:/usr/share/nginx/html nginx
+fi
+
