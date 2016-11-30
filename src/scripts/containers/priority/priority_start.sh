033a28e3956ee82a6dbc30a49a1374e653a5dadc
Unrealwork <unrealwork@gmail.com>
2016-11-30 13:20:50 +0300

    fix commands

--- src/scripts/containers/priority/priority_start.sh
+++ src/scripts/containers/priority/priority_start.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 rm -rf ~/myblog
-jekkyl new myblog
+jekyll new myblog
 docker rm -f docker-nginx
 docker run --name docker-nginx -p 80:80 -d -v ~/myblog/_site/:/usr/share/nginx/html nginx
