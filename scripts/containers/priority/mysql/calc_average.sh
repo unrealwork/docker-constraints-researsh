0f68cdf720bf36eb92a7312f52be0e14fb741d19
Unrealwork <unrealwork@gmail.com>
2016-12-12 11:59:46 +0300

    modules

--- scripts/containers/priority/mysql/calc_average.sh
+++ scripts/containers/priority/mysql/calc_average.sh
@@ -7,24 +7,28 @@ fi;
 
 function calc() { awk "BEGIN{print $*}"; }
 
-#files
-pid_stats_file='/proc/'${pid}'/stat';
-proc_uptime_file='/proc/uptime'
-#extract values
-stats_values=($(<${pid_stats_file}))
-uptime_values=($(<${proc_uptime_file}))
-#variables
-hertz=$(getconf CLK_TCK)
-uptime=(${uptime_values[0]})
-utime=${stats_values[14]}
-stime=${stats_values[15]}
-cutime=${stats_values[16]}
-cstime=${stats_values[17]}
-starttime=${stats_values[22]}
-#calculations
-total_time=$(calc ${utime}+${stime}+${cutime}+${cstime})
-seconds=$(calc -${uptime}+${starttime}/${hertz})
-cpu_usage=$(calc 100*'(('${total_time}/${hertz}')'/${seconds}')')
-echo ${cpu_usage}
+function average_pid_cpu_usage() {
+    #files
+    local pid_stats_file='/proc/'$1'/stat';
+    local proc_uptime_file='/proc/uptime'
+    #extract values
+    local stats_values=($(<${pid_stats_file}))
+    local uptime_values=($(<${proc_uptime_file}))
+    #variables
+    local hertz=$(getconf CLK_TCK)
+    local uptime=(${uptime_values[0]})
+    local utime=${stats_values[14]}
+    local stime=${stats_values[15]}
+    local cutime=${stats_values[16]}
+    local cstime=${stats_values[17]}
+    local starttime=${stats_values[22]}
+    #calculations
+    local total_time=$(calc ${utime}+${stime}+${cutime}+${cstime})
+    local seconds=$(calc -${uptime}+${starttime}/${hertz})
+    local cpu_usage=$(calc 100*'(('${total_time}/${hertz}')'/${seconds}')')
+    echo ${cpu_usage}
+}
+
+average_pid_cpu_usage ${pid}
 
 
