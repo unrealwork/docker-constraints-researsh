a8fdc6cec3e9351ef45b995f67a15da225d503c9
Unrealwork <unrealwork@gmail.com>
2016-12-20 16:52:04 +0300

    fix jmeter job

--- scripts/containers/normal/stressed_schedule.sh
+++ scripts/containers/normal/stressed_schedule.sh
@@ -11,9 +11,9 @@ function docker_container_exist {
 }
 
 function clean_up() {
-    if [[ $(docker_container_exist ${container_name}) ]]; then
-        docker rm -vf ${container_name}
-    fi
+    if docker_container_exist ${container_name}; then
+    docker rm -f ${container_name}
+fi
 }
 
 function get_current_mills {
@@ -21,12 +21,23 @@ function get_current_mills {
     exit 1;
 }
 
+function send_statistic {
+    echo -e "property e:stressed-configuration k:id=$1 t:cpu  ms:"$(get_current_mills)" v:start_time="$2" v:end_time="$3" v:options="$4> /dev/tcp/hbs.axibase.com/9081
+}
+
+
 core_count=8;
+clean_up
 for ((i = 1; i <= $core_count; i++)); do
-    docker run --name ${container_name}  -it progrium/stress --cpu ${i}
+    echo ${i}" configuration is started";
+    options="--cpu "${i}"";
+    docker run --name ${container_name} -it progrium/stress ${options} &
+    echo "Warm up!";
     sleep 10;
     start_time=$(get_current_mills)
-    sleep 900000;
+    sleep 10;
     clean_up
-    end_time$(get_current_mills)
+    end_time=$(get_current_mills)
+    echo "Sending statistic about the configuration";
+    send_statistic conf${i} ${start_time} ${end_time} ${options}
 done
