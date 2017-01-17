55dad5da70c43538fe412707c10d9642cdbaac7d
Unrealwork <unrealwork@gmail.com>
2017-01-17 16:46:42 +0300

    fix

--- scripts/containers/normal/stressed_schedule.sh
+++ scripts/containers/normal/stressed_schedule.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 container_name=stressed;
+constraint=$3
 
 function docker_container_exist {
     if [ -z "$(docker ps -a  | grep $1)" ]; then
@@ -34,7 +35,8 @@ function send_configuration_entity {
     send_network_command ${command}
 }
 function send_configuration_property {
-    local command="property e:stressed-configurations k:id=$1 t:cpu  d:"'"'$(get_current_iso_time)'"'" v:start_time="$2" v:end_time="$3" v:options="'"'$4'"';
+    suffix=$5
+    local command="property e:stressed-configurations"${suffix}" k:id=$1 t:cpu  d:"'"'$(get_current_iso_time)'"'" v:start_time="$2" v:end_time="$3" v:options="'"'$4'"';
     send_network_command ${command}
 }
 
@@ -47,7 +49,7 @@ for ((i = 0; i <= $core_count; i++)); do
     echo "Warm up!";
     start_time=$(get_current_iso_time);
     if [[ ${i} > 0 ]]; then
-        docker run --name ${container_name} -it progrium/stress ${options} --timeout $(calc $1+$2)
+        docker run --name ${container_name} ${constraint} -it progrium/stress ${options} --timeout $(calc $1+$2)
     else
         echo "Just sleep without loading";
         sleep $(calc $1+$2)
@@ -57,4 +59,23 @@ for ((i = 0; i <= $core_count; i++)); do
     echo ${end_time}
     echo "Sending statistic about the configuration";
     send_configuration_property conf${i} ${start_time} ${end_time} "$options"
+done
+
+clean_up
+for ((i = 0; i <= $core_count; i++)); do
+    echo ${i}" constraint configuration is started";
+    options="--cpu "${i}"";
+    echo "Warm up!";
+    start_time=$(get_current_iso_time);
+    if [[ ${i} > 0 ]]; then
+        docker run --name ${container_name} ${constraint} -it progrium/stress ${options} --timeout $(calc $1+$2)
+    else
+        echo "Just sleep without loading";
+        sleep $(calc $1+$2)
+    fi;
+    clean_up
+    end_time=$(get_current_iso_time)
+    echo ${end_time}
+    echo "Sending statistic about the configuration";
+    send_configuration_property conf${i} ${start_time} ${end_time} "$options" "-constraint"
 done
